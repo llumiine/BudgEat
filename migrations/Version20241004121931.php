f6c37f5de914d4c9bccedc30acc56a00616896c4
SoroMilena25 <soro.milena25@gmail.com>
2024-10-04 14:31:01 +0200

    'xgk:!

---
+++ migrations/Version20241004121931.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241004121931 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE restaurants ADD mail VARCHAR(255) NOT NULL, ADD mdp VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE restaurants DROP mail, DROP mdp');
+    }
+}
